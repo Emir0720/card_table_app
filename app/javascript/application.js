c28c85b48c6510bb0311dd10756b433607fb8689
Emir0720 <aliemirr.455687@gmail.com>
2024-08-14 17:05:48 +0300

    The column positioning has been updated.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -19,20 +19,21 @@ window.onclick = function(event) {
 }
 
 
-// window.openModal = function(modalId) {
-//     var modal = document.getElementById(modalId);
-//     modal.style.display = "block";
-//   }
-  
-//   window.closeModal = function(modalId) {
-//     var modal = document.getElementById(modalId);
-//     modal.style.display = "none";
-//   }
-  
-//   
-//   window.onclick = function(event) {
-//     if (event.target.classList.contains('modal')) {
-//       window.closeModal(event.target.id);
-//     }
-//   }
+document.addEventListener("DOMContentLoaded", () => {
+  const triageColumn = document.querySelector(".list[data-id='9']");
+  const notNowColumn = document.getElementById("not-now-column");
+
+  if (triageColumn && notNowColumn) {
+    const updateNotNowHeight = () => {
+      
+      if (triageColumn.offsetHeight) {
+        notNowColumn.style.height = `${triageColumn.offsetHeight}px`;
+      }
+    };
+    updateNotNowHeight();
+    window.addEventListener("resize", updateNotNowHeight);
+    document.addEventListener("card:updated", updateNotNowHeight);
+  }
+});
+
   
